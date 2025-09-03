f75f7dceda37f9759f03614ad6ea987bcb89e708
surajm17799-hue <surajm17799@gmail.com>
2025-09-03 16:46:05 +0530

    Update auth_gdrive.py

--- auth_gdrive.py
+++ auth_gdrive.py
@@ -1,7 +1,1 @@
-from pydrive2.auth import GoogleAuth
-from pydrive2.drive import GoogleDrive
 
-def get_gdrive():
-    gauth = GoogleAuth()
-    gauth.LocalWebserverAuth()  # Opens a browser for OAuth
-    return GoogleDrive(gauth)
